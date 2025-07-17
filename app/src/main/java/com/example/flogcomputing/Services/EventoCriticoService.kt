57bbaa15a75f4daef4647c9ebcb4426ca6533f95
Gatito027 <dddanieeel@hotmail.com>
2025-07-16 18:24:09 -0600

    Actualizando alertas

--- app/src/main/java/com/example/flogcomputing/Services/EventoCriticoService.kt
+++ app/src/main/java/com/example/flogcomputing/Services/EventoCriticoService.kt
@@ -29,21 +29,73 @@ class LocationHelper(private val context: Context) {
 
 class EventoCriticoService (private val context: Context) {
     private lateinit var dbHelper: EventosCriticosDataBaseHelper
+    private lateinit var dbHistorial: MedicionesDataBaseHelper
     @RequiresApi(Build.VERSION_CODES.O)
     suspend fun Monitorizarparametros(medicion: medicion) {
+        dbHelper = EventosCriticosDataBaseHelper(context)
+        dbHistorial = MedicionesDataBaseHelper(context)
+        val Historial4Hrs = dbHistorial.getMedicionesUltimas4Horas()
+        var sinMovimiento = 0
+        var enviarDatos : Boolean = false
+        var data = eventoCritico(
+            usuarioId = medicion.usuarioId,
+            fechaHora = LocalDateTime.now(),
+            tipoEvento = "",
+            ubicacion = "",
+            respuestaUsu = "")
+        val locationHelper = LocationHelper(context)
+        val location = locationHelper.getLastKnownLocation()
+        val ubicacionStr = location?.let { "${it.latitude}, ${it.longitude}" } ?: "No disponible"
+        if (Historial4Hrs.isNotEmpty()) {
+            // Iterar sobre cada elemento del historial
+            for (registro in Historial4Hrs) {
+                // Verificar si el registro tiene el campo actividadFisica y si es igual a 0
+                if (registro.actividadFisica.toInt() == 0) {
+                    sinMovimiento++
+                }
+            }
+        }
+            if (Historial4Hrs.size == sinMovimiento) {
+                data = eventoCritico(
+                    usuarioId = medicion.usuarioId,
+                    fechaHora = LocalDateTime.now(),
+                    tipoEvento = "No se detecto movimiento",
+                    ubicacion = ubicacionStr,
+                    respuestaUsu = "Mas de 4 horas sin movimiento"
+                )
+                enviarDatos = true
+            }
         if (medicion.frecuenciaCardiaca >= 160 || medicion.frecuenciaCardiaca <= 40) {
-            dbHelper = EventosCriticosDataBaseHelper(context)
-            val locationHelper = LocationHelper(context)
-            val location = locationHelper.getLastKnownLocation()
-            val ubicacionStr = location?.let { "${it.latitude}, ${it.longitude}" } ?: "No disponible"
-
-            val data = eventoCritico(
+            data = eventoCritico(
                 usuarioId = medicion.usuarioId,
                 fechaHora = LocalDateTime.now(),
                 tipoEvento = "Frecuencia Cardiaca anormal",
                 ubicacion = ubicacionStr,
                 respuestaUsu = "Frecuencia cardiaca: ${medicion.frecuenciaCardiaca}"
             )
+            enviarDatos=true
+        }
+        if(medicion.saturacionOxigeno <= 80){
+            data = eventoCritico(
+                usuarioId = medicion.usuarioId,
+                fechaHora = LocalDateTime.now(),
+                tipoEvento = "Saturacion de oxigeno baja",
+                ubicacion = ubicacionStr,
+                respuestaUsu = "Saturacion de oxigeno: ${medicion.saturacionOxigeno}"
+            )
+            enviarDatos=true
+        }
+        if(medicion.variabilidadFrecuenciaCardiaca <= 0.4 ){
+            data = eventoCritico(
+                usuarioId = medicion.usuarioId,
+                fechaHora = LocalDateTime.now(),
+                tipoEvento = "Vaiabilidad en la frecuencia cardiaca baja",
+                ubicacion = ubicacionStr,
+                respuestaUsu = "Variabilidad de la frecuencia cardiaca: ${medicion.variabilidadFrecuenciaCardiaca}"
+            )
+            enviarDatos=true
+        }
+        if(enviarDatos){
             dbHelper.addEventoCritico(data)
             // TODO: Implementar lógica para la nube
         }
